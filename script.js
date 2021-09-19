2e0c73767b91a72b380fea132784fdfdd3cab426
DarrickPrichett <darrickprichett@gmail.com>
2021-09-18 21:35:38 -0500

    added js to create forecast days

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const weatherApiRootUrl = 'https://api.openweathermap.org';
 const weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
 let city = document.querySelector('#searchTerm').value;
 const forecastElement = document.querySelector('#forecast');
-const now = new Date();
+const present = new Date();
+const todayElement = document.querySelector('#today');
 let cities = [];
 
 function displayCurrent(current) {
@@ -10,6 +11,12 @@ function displayCurrent(current) {
 
 }
 
+function buildFiveDay(date, days) {
+  let dayArray = new Date(date);
+  dayArray.setDate(dayArray.getDate() + days);
+  dayArray = body.current.dt(dayArray);
+  return dayArray;
+}
 function displayFiveDay(daily) {
   console.log(daily)
   let forecastName = document.querySelector('#forecastName');
@@ -20,7 +27,7 @@ function displayFiveDay(daily) {
     cardElement.classList.add("card", "bg-primary");
     let cardHeader = document.createElement("h4");
     cardHeader.classList.add("card-title");
-    cardHeader.textContent=addDays(now, i);
+    cardHeader.textContent=buildFiveDay(present, i);
     cardElement.appendChild(cardHeader)
     forecastElement.appendChild(cardElement)
   }
@@ -51,22 +58,22 @@ function searchCityWeather() {
     const current = body.current;
     const daily = body.daily;
     displayCurrent(current);
-    var today=document.getElementById("today")
+    var todayElement=document.getElementById("today")
     var cityName=document.createElement("h1")
-    cityName.textContent=city +" "+ moment().subtract(10, 'days').calendar();
-    today.append(cityName)
+    cityName.textContent=city +" " + moment().format('MM DD YYYY');
+    todayElement.append(cityName)
     var temp=document.createElement("p")
     temp.textContent="temp: " + body.current.temp +" F"
     today.append(temp)
     var wind=document.createElement("p")
     wind.textContent="wind: " + body.current.wind_speed +" MPH"
-    today.append(wind)
+    todayElement.append(wind)
     var humidity=document.createElement("p")
     humidity.textContent="humidity: " + body.current.humidity+" %"
-    today.append(humidity)
+    todayElement.append(humidity)
     var uvi=document.createElement("p")
     uvi.textContent="UV-Index: " + body.current.uvi
-    today.append(uvi)
+    todayElement.append(uvi)
     displayFiveDay(daily);
     saveToLocalState(city);
   })
